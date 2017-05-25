fa7fea8d505dfc4b94f184024fa4653eb8c4a2e3
rahulyadav <rahulyadav170923@gmail.com>
2017-05-25 16:21:02 +0530

    CORS support for /oauth and /api(we do not get clientId so all the domains will be allowed  ) routes

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const express = require('express')
     , passport = require('./passport/passporthandler')
     , path = require('path')
     , cookieParser = require('cookie-parser')
-    , exphbs = require('express-hbs');
+    , exphbs = require('express-hbs')
+    , cors = require('cors');
 
 const secrets = require('./secrets.json')
     , config = require('./config')
@@ -17,9 +18,41 @@ const secrets = require('./secrets.json')
     , signuprouter = require('./routers/signup')
     , apirouter = require('./routers/api')
     , oauthrouter = require('./routers/oauthrouter')
-    , pagerouter = require('./routers/pagerouter');
+    , pagerouter = require('./routers/pagerouter')
+    , Client = require('./db/models').models.Client;
 
 const app = express();
+
+function getHostName(url) {
+    var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+    if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+    return match[2];
+    }
+    else {
+        return null;
+    }
+}
+
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  Client.findById(req.query.client_id).then(data=>{
+    var domain = data.dataValues.domain;
+    console.log(domain)
+    var found = false;
+    if(domain.map(function(url){return getHostName(url)}).indexOf(getHostName(req.header('Origin')))!==-1){
+        found = true;
+    }
+    if(found){
+        corsOptions = { origin: true }
+    }
+    else{
+        corsOptions = { origin: false }
+    }
+    callback(null, corsOptions)
+});
+};
+
+
 const redirectToHome = function (req, res, next) {
 
     if (req.path == '/') {
@@ -56,8 +89,8 @@ app.use('/login', loginrouter);
 app.use('/connect', connectrouter);
 app.use('/logout', logoutrouter);
 app.use('/signup', signuprouter);
-app.use('/api', apirouter);
-app.use('/oauth', oauthrouter);
+app.use('/api', cors(), apirouter);
+app.use('/oauth', cors(corsOptionsDelegate), oauthrouter);
 app.use('/', pagerouter);
 
 app.listen(3838, function () {
